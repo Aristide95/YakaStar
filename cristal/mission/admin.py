1323b4213cabd98e8d2eae883753b26514a663b1
root <root@localhost.localdomain>
2018-04-24 14:31:01 +0200

    [BDD] Base de données terminée

---
+++ cristal/mission/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Mission)
+admin.site.register(Techno)
+admin.site.register(Etudiant)
+admin.site.register(Calendrier)
+# Register your models here.
