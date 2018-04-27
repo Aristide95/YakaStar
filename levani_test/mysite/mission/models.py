10ea7bae7026b8e7d0c06eb0e3cf762300f10f59
Levani Tevdoradze <levani.tevdoradze@epita.fr>
2018-04-27 19:41:38 +0200

    Added num presta

--- levani_test/mysite/mission/models.py
+++ levani_test/mysite/mission/models.py
@@ -3,47 +3,48 @@ from django import forms
 from django.utils import timezone
 
 class Mission(models.Model):
-	title = models.CharField(max_length = 64)
-	desc = models.TextField()
-	type_mission = models.CharField(max_length = 64)
-	state = models.CharField(max_length = 64)
-	creation_date = models.DateTimeField(default = timezone.now)
-	publication_date = models.DateTimeField(null=True)
-	commercial_id = models.ForeignKey('Etudiant', on_delete=models.CASCADE)
-	client_name = models.CharField(max_length = 64)
-	logo_url = models.URLField()
-	devis_url = models.URLField()
-	techno = models.ManyToManyField('Techno')
+    title = models.CharField(max_length = 64)
+    desc = models.TextField()
+    type_mission = models.CharField(max_length = 64)
+    state = models.IntegerField(default = 0)
+    creation_date = models.DateTimeField(default = timezone.now)
+    publication_date = models.DateTimeField(null=True)
+    commercial_id = models.ForeignKey('Etudiant', on_delete=models.CASCADE)
+    client_name = models.CharField(max_length = 64)
+    logo_url = models.URLField()
+    devis_url = models.URLField()
+    techno = models.ManyToManyField('Techno')
+    num_presta = models.IntegerField()
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 class Techno(models.Model):
-	name = models.CharField(max_length = 64)
+    name = models.CharField(max_length = 64)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Etudiant(models.Model):
-	firstname = models.CharField(max_length = 64)
-	lastname = models.CharField(max_length = 64)
-	email = models.EmailField(max_length = 64)
-	status = models.CharField(max_length = 64)
-	cv_url = models.URLField(null=True)
-	creation_date = models.DateTimeField(default = timezone.now)
+    firstname = models.CharField(max_length = 64)
+    lastname = models.CharField(max_length = 64)
+    email = models.EmailField(max_length = 64)
+    status = models.CharField(max_length = 64)
+    cv_url = models.URLField(null=True)
+    creation_date = models.DateTimeField(default = timezone.now)
 
-	def __str__(self):
-		return self.firstname
+    def __str__(self):
+        return self.firstname
 
 class Calendrier(models.Model):
-	begin_hour = models.DateTimeField()
-	duration = models.TimeField()
+    begin_hour = models.DateTimeField()
+    duration = models.TimeField()
 
-	def __str__(self):
-		return str(self.creneau)
+    def __str__(self):
+        return str(self.creneau)
 
 class MissionForm(forms.ModelForm):
     class Meta:
         model = Mission
-        exclude = ['creation_date', 'publication_date']
+        exclude = ['creation_date', 'publication_date', 'state']
 # Create your models here.
