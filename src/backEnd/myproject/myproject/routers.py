0016b48383047c5ccee2f13f600413a97e308572
Kimelyne <kimelyne.servais@epita.fr>
2018-05-27 22:05:23 +0200

    [BACK] add connection

--- src/backEnd/myproject/myproject/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from mission.viewsets import MissionViewSet, EtudiantViewSet, TechnoViewSet, CalendrierViewSet
-
-router = routers.DefaultRouter()
-
-router.register(r'mission', MissionViewSet)
-router.register(r'etudiant', EtudiantViewSet)
-router.register(r'techno', TechnoViewSet)
-router.register(r'calendrier', CalendrierViewSet)
