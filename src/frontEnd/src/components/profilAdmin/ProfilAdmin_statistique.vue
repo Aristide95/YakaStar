221b808e7532f0447583da301070bdb441b6a552
Kimelyne <kimelyne.servais@epita.fr>
2018-06-08 14:40:00 +0200

    [ADD] export stat ok

--- src/frontEnd/src/components/profilAdmin/ProfilAdmin_statistique.vue
+++ src/frontEnd/src/components/profilAdmin/ProfilAdmin_statistique.vue
@@ -21,12 +21,12 @@
         <h3 class="titre">Total des missions : {{missions.length}}</h3>
         <b-row>
           <b-col sm="4">
-            <h6 class="titre">Nombre de mission réalisée : {{termine.length}}</h6>
+            <h6 class="titre">Nombre de missions réalisées : {{termine.length}}</h6>
             <div class="pie">
               <pie-chart
                 :percent='t'
                 :stroke-width=1
-                :label='t'
+                :abel='tString'
                 label-small=""
                 color=#40a070
                 :opacity=0.7
@@ -34,12 +34,12 @@
             </div>
           </b-col>
           <b-col sm="4">
-            <h6 class="titre">Nombre de mission en cours : {{enCours.length}}</h6>
+            <h6 class="titre">Nombre de missions en cours : {{enCours.length}}</h6>
             <div class="pie">
               <pie-chart
                 :percent='e'
                 :stroke-width=1
-                :label='e'
+                :abel='eString'
                 label-small=""
                 color=#40a070
                 :opacity=0.7
@@ -47,12 +47,12 @@
             </div>
           </b-col>
           <b-col sm="4">
-            <h6 class="titre">Nombre de mission annulée : {{annule.length}}</h6>
+            <h6 class="titre">Nombre de missions annulées : {{annule.length}}</h6>
             <div class="pie">
               <pie-chart
                 :percent='a'
                 :stroke-width=1
-                :abel='a'
+                :abel='aString'
                 label-small=""
                 color=#40a070
                 :opacity=0.7
@@ -69,7 +69,7 @@
               <pie-chart
                 :percent='c'
                 :stroke-width=1
-                :label='c'
+                :abel='cString'
                 label-small=""
                 color=#407fa0
                 :opacity=0.7
@@ -82,7 +82,7 @@
               <pie-chart
                 :percent='m'
                 :stroke-width=1
-                :label='m'
+                :abel='mString'
                 label-small=""
                 color=#407fa0
                 :opacity=0.7
@@ -90,12 +90,12 @@
             </div>
           </b-col>
           <b-col sm="4">
-            <h6 class="titre">Nombre de prestataire en mission: {{nb_presta}}</h6>
+            <h6 class="titre">Nombre de prestataire en mission : {{nb_presta.length}}</h6>
             <div class="pie">
               <pie-chart
                 :percent='n'
                 :stroke-width=1
-                :abel='n'
+                :abel='nString'
                 label-small=""
                 color=#407fa0
                 :opacity=0.7
@@ -113,10 +113,12 @@
 </template>
 
 <script>
+/* eslint-disable new-cap */
 import Nav from '../Nav'
 import Footer from '../Footer'
 import axios from 'axios'
 import PieChart from 'vue-pie-chart/src/PieChart.vue'
+import jsPDF from 'jsPDF'
 export default {
   name: 'ProfilAdmin_statistique',
   components: {Footer, Nav, 'pie-chart': PieChart},
@@ -128,11 +130,17 @@ export default {
       enCours: [],
       termine: [],
       a: 0,
+      aString: '0',
       e: 0,
+      eString: '0',
       t: 0,
+      tString: '0',
       c: 0,
+      cString: '0',
       m: 0,
+      mString: '0',
       n: 0,
+      nString: '0',
       nb_presta: 0,
       com: [],
       mvp: [],
@@ -158,8 +166,22 @@ export default {
   mounted: function () {
     this.getStudent()
     this.getMissions()
+    this.getPresta_mission()
   },
   methods: {
+    getPresta_mission: function () {
+      let apirUrl = 'http://127.0.0.1:8000/api/presta_mission/'
+      this.loading = true
+      axios.get(apirUrl)
+        .then((response) => {
+          this.nb_presta = response.data
+          this.loading = false
+        })
+        .catch((err) => {
+          this.loading = false
+          console.log(err)
+        })
+    },
     sortStudents: function (students) {
       for (var i = 0; i < students.length; i++) {
         if (students[i].status === 'com') {
@@ -172,9 +194,15 @@ export default {
       }
       if (this.com.length !== 0) {
         this.c = this.com.length / this.student.length * 100
+        this.cString = this.c.toString()
       }
       if (this.mvp.length !== 0) {
         this.m = this.mvp.length / this.student.length * 100
+        this.mString = this.m.toString()
+      }
+      if (this.nb_presta.length !== 0) {
+        this.n = this.nb_presta.length / this.student.length * 100
+        this.nString = this.n.toString()
       }
     },
     getMissions: function () {
@@ -205,17 +233,21 @@ export default {
         }
       }
       if (this.termine.length !== 0) {
-        this.t = this.termine.length / this.missions.length * 100
+        this.t = this.termine.length / missions.length * 100
+        this.tString = this.t.toString()
       }
       if (this.annule.length !== 0) {
-        this.a = this.annule.length / this.missions.length * 100
+        this.a = this.annule.length / missions.length * 100
+        this.aString = this.a.toString()
       }
       if (this.enCours.length !== 0) {
-        this.e = this.enCours.length / this.missions.length * 100
+        this.e = this.enCours.length / missions.length * 100
+        this.eString = this.e.toString()
       }
 
       if (this.nb_presta !== 0) {
         this.n = this.nb_presta / this.student.length * 100
+        this.nString = this.n.toString()
       }
     },
     getStudent: function () {
@@ -231,6 +263,32 @@ export default {
           this.loading = false
           console.log(err)
         })
+    },
+    createPDF () {
+      let pdfName = 'statistique_CRISTAL'
+      var doc = new jsPDF()
+
+      doc.text('STATISTIQUES', 85, 10)
+      doc.text('MISSIONS', 10, 20)
+      doc.text('Nombre de missions : ' + this.missions.length, 20, 30)
+      doc.text('Nombre de missions réalisées : ' + this.termine.length + ' soit ' + this.t + '%', 20, 40)
+      doc.text('Nombre de missions en cours : ' + this.enCours.length + ' soit ' + this.e + '%', 20, 50)
+      doc.text('Nombre de missions annulées : ' + this.annule.length + ' soit ' + this.a + '%', 20, 60)
+
+      doc.text('PRESTATAIRES', 10, 80)
+      doc.text('Nombre de prestataires : ' + this.student.length, 20, 90)
+      doc.text('Nombre de commerciaux : ' + this.com.length + ' soit ' + this.c + '%', 20, 100)
+      doc.text('Nombre de MVP : ' + this.mvp.length + ' soit ' + this.t + '%', 20, 110)
+      doc.text('Nombre de prestataire en mission : ' + this.nb_presta.length + ' soit ' + this.n + '%', 20, 120)
+
+      doc.text('Meilleurs prestataires', 10, 140)
+
+      for (var i = 0; i < this.mvp.length; i++) {
+        doc.text(this.mvp[i].lastname, 20, 150 + i * 10)
+        doc.text(this.mvp[i].firstname, 80, 150 + i * 10)
+      }
+
+      doc.save(pdfName + '.pdf')
     }
   }
 }
