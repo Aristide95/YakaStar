59b0bb550792434aebeca60051063a737f0f75e1
Kimelyne <kimelyne.servais@epita.fr>
2018-06-27 18:42:31 +0200

    [DELETE] oldversion

--- oldVersion/mission/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^add_mission$', views.add_mission, name='add_mission'),
-    url(r'^delete_mission/(?P<mission_id>[0-9]*)/$', views.delete_mission, name='delete_mission'),
-]
